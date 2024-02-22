e17e42c7ba0dc20de8aefd76303888cb59e38142
Le Doan Duong <leduong04st@gmail.com>
2024-02-23 01:20:35 +0700

    comment explain

--- src/chess_ant/isValidMove.java
+++ src/chess_ant/isValidMove.java
@@ -1,12 +1,12 @@
 package chess_ant;
+
+//file này kiểm tra tính hợp lê của nước đi qua hàm isValidMove(int startX, int startY, int endX, int endY, String[][] board)
 public class isValidMove {
-    // static String[][] board = ReadBoardFromFile.ReadBoardFromFile();
 
     public static boolean isValidMove(int startX, int startY, int endX, int endY, String[][] board) {
         String s = board[startX][startY];
         if (s.equals("R") || s.equals("r") || s.equals("Q") || s.equals("q")) {
             if (Xe_Hau(s, startX, startY, endX, endY, board)) {
-                // System.out.println("Xe or Hau");
                 return true;
             }
 
@@ -15,28 +15,24 @@ public class isValidMove {
 
         if (s.equals("B") || s.equals("b") || s.equals("Q") || s.equals("q")) {
             if (Cheo(s, startX, startY, endX, endY, board) && Tuong_Hau(s, board)) {
-                // System.out.println("Tuong or Hau");
                 return true;
             }
         }
 
         if (s.equals("n") || s.equals("N")) {
             if (Ma(s, startX, startY, endX, endY, board)) {
-                // System.out.println("Ma");
                 return true;
             }
         }
 
         if (s.equals("K") || s.equals("k")) {
             if (Vua(s, startX, startY, endX, endY, board)) {
-                // System.out.println("King");
                 return true;
             }
         }
 
         if (s.equals("p") || s.equals("P")) {
             if (Tot(s, startX, startY, endX, endY, board)) {
-                // System.out.println("Tot");
                 return true;
             }
         }
@@ -46,7 +42,6 @@ public class isValidMove {
 
     public static boolean Xe_Hau(String s, int startX, int startY, int endX, int endY, String[][] board) {
 
-        // if (s == "Q" || s == "q" || s == "r" || s == "R") {
         if (s.equals("Q") || s.equals("q") || s.equals("r") || s.equals("R")) {
             if (Math.abs(startX - endX) != 0 && Math.abs(startY - endY) == 0
                     || Math.abs(startX - endX) == 0 && Math.abs(startY - endY) != 0) {
@@ -54,16 +49,12 @@ public class isValidMove {
                     int x = startX;
                     int y = startY;
 
-                    // System.out.println(KhacPhe(s, board[endX][endY]));
 
                     if (startX > endX) {
-                        // System.out.println(0.1);
                         while (x > endX + 1) {
                             x--;
-                            // System.out.println("ĐMM");
                             if (!board[x][y].equals("| |")) {
-                                // System.out.println("board[x][y]: "+board[x][y]);
-                                // System.out.println("1");
+                                
                                 return false;
                             }
 
@@ -73,11 +64,9 @@ public class isValidMove {
                     }
 
                     if (startX < endX) {
-                        // System.out.println(0.2);
                         while (x < endX - 1) {
                             x++;
                             if (!board[x][y].equals("| |")) {
-                                // System.out.println(2);
                                 return false;
                             }
                         }
@@ -85,11 +74,9 @@ public class isValidMove {
                     }
 
                     if (startY > endY) {
-                        // System.out.println(0.3);
                         while (y > endY + 1) {
                             y--;
                             if (!board[x][y].equals("| |")) {
-                                // System.out.println(3);
                                 return false;
                             }
                         }
@@ -97,11 +84,9 @@ public class isValidMove {
                     }
 
                     if (startY < endY) {
-                        // System.out.println(0.4);
                         while (y < endY - 1) {
                             y++;
                             if (!board[x][y].equals("| |")) {
-                                // System.out.println(4);
                                 return false;
                             }
                         }
@@ -120,10 +105,8 @@ public class isValidMove {
             int x = startX;
             int y = startY;
 
-            // if ((board[endX][endY].equals(" ") || KhacPhe(s, board[endX][endY]))) {
             if (KhacPhe(s, board[endX][endY]) || board[endX][endY].equals("| |")) {
 
-                // System.out.println("Khong cung phe");
                 if (startX > endX && startY > endY) {
                     while (x != endX + 1) {
                         x--;
@@ -169,9 +152,7 @@ public class isValidMove {
                     return true;
                 }
             }
-            // System.out.println("Kiem tra xem co khac phe voi la o trong ko");
         }
-        // System.out.println("Kiem tra xem vi tri co nam tren duong cheo ko");
         return false;
     }
 
@@ -187,7 +168,6 @@ public class isValidMove {
 
         if (s.equals("N") || s.equals("n")) {
             if (KhacPhe(s, board[endX][endY]) || board[endX][endY].equals("| |")) {
-                // System.out.println(1);
                 if (Math.abs(startX - endX) == 1 && Math.abs(startY - endY) == 2
                         || Math.abs(startX - endX) == 2 && Math.abs(startY - endY) == 1) {
                     
@@ -214,9 +194,7 @@ public class isValidMove {
                     return true;
                 }
 
-                // if (Math.abs(startX - endX) == 1 && Math.abs(startY - endY) == 1) {
-                // return true;
-                // }
+
 
             }
         }
@@ -225,11 +203,8 @@ public class isValidMove {
 
     public static boolean Tot(String s, int startX, int startY, int endX, int endY, String[][] board) {
         if (s.equals("p")) {
-            // System.out.println(1);
             if (KhacPhe(s, board[endX][endY]) || board[endX][endY].equals("| |")) {
-                // System.out.println(2);
                 if (endX - startX == -2 && startX == 6 && startY == endY && board[endX][endY].equals("| |")) {
-                    // System.out.println(3);
                     if (board[startX - 1][startY].equals("| |")) {
                         return true;
                     }
@@ -285,7 +260,6 @@ public class isValidMove {
             for (int j = 0; j < 8; j++) {
 
                 if (board[i][j].equals("| |")) {
-                    // System.out.print(" ");
                     System.out.print("  ");
                 }
 
