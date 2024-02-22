52135c40388bd1f37addfae4e16d1aa92032fcb2
Le Doan Duong <leduong04st@gmail.com>
2024-02-23 00:59:54 +0700

    xóa comment ChessBoardGUI.java

--- src/chess_ant/ChessBoardGUI.java
+++ src/chess_ant/ChessBoardGUI.java
@@ -1,32 +1,3 @@
-// package chess_ant;
-// import javax.swing.JOptionPane;
-
-// public class ChessBoardGUI {
-//     public static void main(String[] args) {
-//         int bot;
-//         boolean playersChoice = false; // Ban đầu, không có lựa chọn của người chơi
-//         while (!playersChoice) { // Lặp cho đến khi người chơi chọn màu quân
-//             String[] options = { "Trắng", "Đen" };
-//             int result = JOptionPane.showOptionDialog(null, "Chọn màu quân cờ bạn muốn chơi", "Chọn màu quân cờ",
-//                     JOptionPane.YES_NO_OPTION, JOptionPane.QUESTION_MESSAGE, null, options, options[0]);
-//             if (result == JOptionPane.YES_OPTION) {
-//                 bot = 1;
-//                 Player.main(1);
-//                 Bot.Bot(1);
-//                 playersChoice = true; // Đặt cờ để thoát khỏi vòng lặp
-//             } else if (result == JOptionPane.NO_OPTION) {
-//                 bot = -1;
-//                 Player.main(-1);
-//                 Bot.Bot(-1);
-//                 playersChoice = true; // Đặt cờ để thoát khỏi vòng lặp
-//             } else {
-//                 JOptionPane.showMessageDialog(null, "Vui lòng chọn màu quân cờ!");
-//             }
-//         }
-//     }
-// }
-
-
 package chess_ant;
 
 import javax.swing.JOptionPane;
