c1b9bb8108d6eb0a8c087fb97e3f4e5457c2bca9
Le Doan Duong <leduong04st@gmail.com>
2024-03-25 23:25:29 +0700

    abc

--- src/chess_ant/makeMove.java
+++ src/chess_ant/makeMove.java
@@ -17,7 +17,7 @@ public class makeMove {
     // }
 
 
-    public static void makeMove(int fromRow, int fromCol, int toRow, int toCol, String[][] board, int bot) {
+    public static boolean makeMove(int fromRow, int fromCol, int toRow, int toCol, String[][] board, int bot) {
         if(whoWon.displayWinner(ReadBoardFromFile.ReadBoardFromFile())==0)
         {
             if(checkSide(board[fromRow][fromCol], bot)) {
@@ -25,6 +25,7 @@ public class makeMove {
                     board[toRow][toCol] = board[fromRow][fromCol];
                     board[fromRow][fromCol] = "| |";
                     ChessNotation.ChessNotation(fromRow, fromCol, toRow, toCol, ChessNotation.readFromFile("src\\chess_ant\\Notation.txt"));
+                    return true;
                 } else {
                     JOptionPane.showMessageDialog(null, "Nước đi không hợp lệ, hãy thực hiện lại");
                 }
@@ -33,6 +34,7 @@ public class makeMove {
                 
             }
         }
+        return false;
 }
     public static boolean checkSide(String str, int bot) {
         if (str.matches("[A-Z]+") && bot == -1) {
